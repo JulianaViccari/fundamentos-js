64b25a8fa0402f05412b6beb7f4ffae4fd266e01
Juliana Viccari <juliana.z.viccari@gmail.com>
2022-03-19 02:03:36 -0300

    Treinando com ternário

--- ternario.js
+++ ternario.js
@@ -1,14 +1,29 @@
 
-const nome = "Leo";
-const idade = 23;
-const bebidaMaior = "cerveja";
-const bebidaMenor = "suco";
+// const nome = "Leo";
+// const idade = 23;
+// const bebidaMaior = "cerveja";
+// const bebidaMenor = "suco";
 
-// if(idadeCliente >= idadeMinima){
-//   console.log("Pode beber cerveja")
-// }else{
-//   console.log("Suco")
-// }
-                                                       //condição      //true       //false
-const pedido = `${nome} diz: "por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}"`
-console.log(pedido)
+// // if(idadeCliente >= idadeMinima){
+// //   console.log("Pode beber cerveja")
+// // }else{
+// //   console.log("Suco")
+// // }
+//                                                        //condição      //true       //false
+// const pedido = `${nome} diz: "por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}`
+// console.log(pedido)
+
+const nome ="Eduardo";
+const peso = 100;
+const altura = 1.81;
+
+let imc = peso / (altura * altura);
+
+let ideal = 25;
+let sobrePeso = 30;
+let magreza = 18;
+let resultadoOk = "Parabéns você está ótimo!!! ";
+let resultadoRuim = "Cuidado, procure um médico!!!"
+
+const resultado = `${nome} com um peso de: ${peso} klgramas e altura de: ${altura} centímetros seu imc foi: ${imc} ${imc <= ideal && imc <= sobrePeso && imc >= magreza ? resultadoOk : resultadoRuim}`;
+console.log(resultado)
