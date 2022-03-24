bcbddc310981bf3277c6dd265c72c57bc19a0b20
Juliana Viccari <juliana.z.viccari@gmail.com>
2022-03-23 23:28:43 -0300

    Alterações

--- media-foreach.js
+++ media-foreach.js
@@ -9,20 +9,22 @@
 //let media = somaDasNotas/notas.length;
 
 //console.log(media);
-const notas1 = [10, 6.5, 8 ,7.5]
-const notas2 = [9, 6, 6]
-const notas3 = [8.5, 9.5]
+const notas1 = [10, 6.5, 8 ,7.5] 
+const notas2 = [9, 6, 6] 
+const notas3 = [8.5, 9.5] 
                       //10   //9   //7
 const notasGerais = [notas1,notas2,notas3]
 
 let media = 0
 
 for (let i = 0; i < notasGerais.length; i++) {
+  let soma = 0
   for (let j = 0; j < notasGerais[i].length; j++) {
-    media += notasGerais[i][j]/notasGerais[i].length;
+    soma += notasGerais[i][j];
   }
+  media += soma / notasGerais[i].length;
 }
 
-media = media/notasGerais.length
 
-console.log(media)
+
+console.log(media/ notasGerais.length)
--- pontos-extras.js
+++ pontos-extras.js
@@ -1,5 +1,5 @@
 const notas = [10, 6.5, 8 ,9.5]
 
-const notasAtualizadas = notas.map( nota => nota <= 10 ? nota : ++nota)
+const notasAtualizadas = notas.map( nota => nota >= 10 ? nota : ++nota)
 
 console.log(notasAtualizadas)
